855d29fe605efd3e0738b251316c9e610781383a
Rallz1505 <111504114+Rallz1505@users.noreply.github.com>
2023-01-10 11:01:49 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        var max = 4000000;
+        var sum = 0;
+        var num1 = 1;
+        var num2 = 2;
+
+        while (1 == 1)
+        {
+            sum = num1 + num2;
+            num1 = sum;
+            Console.WriteLine(sum);
+            Console.ReadLine();
+            sum = num1 + num2;
+            num2 = sum;
+            Console.WriteLine(sum);
+            Console.ReadLine();
+            if (sum !> 4000000)
+            {
+                break;
+            }
+        }
+
+
+    }
+}
+    
